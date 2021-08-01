2a98cfe0aa506b54846db534b12c20a7cc50b05a
Eiji <willianeijitanaka@gmail.com>
2021-08-01 00:48:17 -0300

    commit inicial

---
+++ typeNumber.js
@@ -0,0 +1,36 @@
+//tipo Number
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+//Nan -> Not a Number (não é um número)
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+
+//return infinity
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
+
+//constante
+const pi = 3.14;
+console.log(pi);
+console.log(Math.PI);
---
+++ typeString.js
@@ -0,0 +1,33 @@
+//tipo String
+const texto1 = "Olá, Mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhasupersegura456!";
+const StringDeNumeros = "34567";
+const citacao = 'O Leo disse "oi"';
+
+console.log(texto1);
+console.log(texto2);
+console.log(senha);
+console.log(StringDeNumeros);
+console.log(citacao);
+
+//concatenação (+)
+const textoNome = "Meu nome é ";
+const meuNome = "Willian";
+console.log(textoNome + meuNome);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3048\u3044\u3058'
+const katakana = '\u30a8\u30a4\u30B8'
+const eiji = "&#12456;&#12452;&#12472;";
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+console.log(katakana)
+
+//template string ou template literal
+
