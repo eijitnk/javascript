98164c15f1851e2f3b70506037793aaa14ec3d30
Eiji <willianeijitanaka@gmail.com>
2021-08-01 12:49:26 -0300

    operadores de funções

---
+++ expressoes.js
@@ -0,0 +1,22 @@
+// function minhaFuncao(param) {
+//   //bloco de codigo
+// }
+
+// minhaFuncao("param");
+
+//expressão de função
+
+//diferença principal: HOSTING
+//funções e var são "listadas" no topo do arquivo.
+
+const soma = function(num1, num2) {
+  return num1 + num2;
+};
+
+console.log(apresentar());
+
+function apresentar() {
+  return "olá";
+}
+
+console.log(soma(1,1));
--- funcoes.js
+++ funcoes.js
@@ -7,12 +7,20 @@ function imprimeTexto(texto) {
   console.log(texto);
 }
 
-imprimeTexto("teste");
+imprimeTexto(soma(2, 5));
 
 //três formas de escrever funções
-function soma() {
-  const resultado = 2 + 3;
-
+function soma(num1 = 0, num2 = 0) {
+  return num1 + num2;
 }
 
-soma();
+function multiplica(num1 = 1, num2 = 1) {
+  return num1 * num2;
+}
+
+function nomeIdade(nome, idade) {
+  return `meu nome é ${nome} e minha idade é ${idade}`;
+}
+
+console.log(nomeIdade(40, "Juliana"));
+console.log(multiplica(soma(4,5), soma(3,3)));
